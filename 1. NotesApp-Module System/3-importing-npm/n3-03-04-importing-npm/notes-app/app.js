54c74d46a5f315c14ae688a5a92d24524ebf97f2
TopsCode <kailash.solanki@tops-int.com>
2020-08-04 13:43:53 +0530

    Update app.js

--- 1. NotesApp-Module System/3-importing-npm/n3-03-04-importing-npm/notes-app/app.js
+++ 1. NotesApp-Module System/3-importing-npm/n3-03-04-importing-npm/notes-app/app.js
@@ -4,4 +4,5 @@ const getNotes = require('./notes.js')
 const msg = getNotes()
 console.log(msg)
 
-console.log(validator.isURL('https/mead.io'))
+console.log(validator.isURL('https://www.tops-int.com/'))
+console.log(validator.isEmail('abc@gmail.com'))
